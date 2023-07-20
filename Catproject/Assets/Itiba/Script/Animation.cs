7ca335a31f1d9bf9ed77711b30670a6655a99a66
ichiba <urata.9489@gmail.com>
2023-07-20 09:04:06 +0900

    アニメーション

---
+++ Catproject/Assets/Itiba/Script/Animation.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+
+public class Animation : MonoBehaviour
+{
+    [Header("‚¤‚Â‚Þ‚­")]
+    public Transform m_CatTransform;
+}
