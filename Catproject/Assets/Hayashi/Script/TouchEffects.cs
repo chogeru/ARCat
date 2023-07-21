467cb6219c02732daef773c20cd9d489fa710a6b
ABURAMUSI <108376848+chogeru@users.noreply.github.com>
2023-07-21 13:22:19 +0900

    追加

---
+++ Catproject/Assets/Hayashi/Script/TouchEffects.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchEffects : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Touch touch = Input.GetTouch(0);
+    }
+}
